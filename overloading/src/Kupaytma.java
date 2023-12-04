360b21160da34e760a13ffc0b70ec61331c47fcf
diyorbek0207 <140248878+diyorbek0207@users.noreply.github.com>
2023-12-04 15:44:32 +0500

    add Kupaytma

--- overloading/src/Kupaytma.java
+++ overloading/src/Kupaytma.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Kupaytma {
+public class Kupaytma {
+    public int multiply(int a, int b){
+        return a*b;
+    }
+    public double multiply(double a,double b){
+        return a*b;
+    }
+
+    public static void main(String[] args) {
+        Kupaytma kupaytma=new Kupaytma();
+        System.out.println(kupaytma.multiply(12,32));
+        System.out.println(kupaytma.multiply(20.1,12.3));
+
+    }
 }
