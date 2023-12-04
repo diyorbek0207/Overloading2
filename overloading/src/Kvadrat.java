42adb1ff913f2766204f89e3afce60d844ee8585
diyorbek0207 <140248878+diyorbek0207@users.noreply.github.com>
2023-12-04 15:45:08 +0500

    add Kvadtrat

--- overloading/src/Kvadrat.java
+++ overloading/src/Kvadrat.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class Kvadrat {
+public class Kvadrat {
+    public double pow(double a){
+        return Math.pow(a,2);
+    }
+
+    public static void main(String[] args) {
+        Kvadrat kvadrat=new Kvadrat();
+        System.out.println(kvadrat.pow(9.2));
+    }
 }
