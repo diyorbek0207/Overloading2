0047e0e9a841816501374f2caf0b9c82eabc582a
diyorbek0207 <140248878+diyorbek0207@users.noreply.github.com>
2023-12-04 15:43:47 +0500

    add Bulinma

--- overloading/src/Bulinma.java
+++ overloading/src/Bulinma.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class Bulinma {
+public class Bulinma {
+    public int div(int a,int b){
+        return a/b;
+    }
+    public double div(double a, double b){
+        return a/b;
+    }
+
+    public static void main(String[] args) {
+        Bulinma bulinma=new Bulinma();
+        System.out.println(bulinma.div(52,41));
+        System.out.println(bulinma.div(14.5,9.32));
+    }
 }
