b7c442e6bd7217bb6491090dd37feddeee55b98f
diyorbek0207 <140248878+diyorbek0207@users.noreply.github.com>
2023-12-04 15:43:13 +0500

    add Ayirma

--- overloading/src/Ayirma.java
+++ overloading/src/Ayirma.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class Ayirma {
+public class Ayirma {
+    public int sub(int a,int b){
+        return a-b;
+    }
+    public double sub(double a,double b){
+        return a-b;
+    }
+
+    public static void main(String[] args) {
+        Ayirma ayirma=new Ayirma();
+        System.out.println(ayirma.sub(9,5));
+        System.out.println(ayirma.sub(12.5,21.4));
+    }
 }
