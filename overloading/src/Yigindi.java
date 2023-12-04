11c788107a2e40a08aabc509c5f632267b66aedb
diyorbek0207 <140248878+diyorbek0207@users.noreply.github.com>
2023-12-04 15:45:39 +0500

    add Yigindi

--- overloading/src/Yigindi.java
+++ overloading/src/Yigindi.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Yigindi {
+public class Yigindi {
+    public int add(int a,int b){
+       return a+b;
+    }
+
+    public double add(double a,double b){
+        return a +b;
+    }
+
+    public static void main(String[] args) {
+        Yigindi yigindi=new Yigindi();
+        System.out.println(yigindi.add(78,45));
+        System.out.println(yigindi.add(14.45,32.14));
+    }
 }
